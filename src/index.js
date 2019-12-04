28c21a26b25c9c7413dcf15bdc2c01513487ce14
Ernesto Moyano <efmoyano@tufix.com.ar>
2019-12-04 16:38:51 -0300

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
 
 const rootElement = document.getElementById('root');
 
 render(<App />, rootElement);
-
-registerServiceWorker();
